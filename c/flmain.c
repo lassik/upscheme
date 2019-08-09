b5946dea7733bd8086f6d8d93bb41c64bd104bd6
Lassi Kortela <lassi@lassi.io>
2019-08-09 20:11:02 +0300

    Remove unused INITFILE config option

--- c/flmain.c
+++ c/flmain.c
@@ -43,9 +43,6 @@ int main(int argc, char *argv[])
     fl_init(512 * 1024);
 
     fname_buf[0] = '\0';
-#ifdef INITFILE
-    strcat(fname_buf, INITFILE);
-#else
     value_t str = symbol_value(symbol("*install-dir*"));
     char *exedir = (str == UNBOUND ? NULL : cvalue_data(str));
     if (exedir != NULL) {
@@ -53,7 +50,6 @@ int main(int argc, char *argv[])
         strcat(fname_buf, PATHSEPSTRING);
     }
     strcat(fname_buf, "flisp.boot");
-#endif
 
     value_t args[2];
     fl_gc_handle(&args[0]);
