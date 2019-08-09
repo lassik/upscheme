7d93f262a78757a8682668ce9b0db84210fe120a
Lassi Kortela <lassi@lassi.io>
2019-08-09 19:50:21 +0300

    Set C compiler options -Wextra -std=gnu99

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -eu
 CC="${CC:-clang}"
-CFLAGS="-O2 -falign-functions -Wall -Wno-strict-aliasing"
+CFLAGS="-Wall -Wextra -Wno-strict-aliasing -std=gnu99"
+CFLAGS="$CFLAGS -O2 -falign-functions"
 CFLAGS="$CFLAGS -I ../c -D NDEBUG -D USE_COMPUTED_GOTO"
 LFLAGS="-lm"
 builddir="build-$(uname | tr A-Z- a-z_)-$(uname -m | tr A-Z- a-z_)"
