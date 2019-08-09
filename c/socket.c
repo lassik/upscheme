2b65fe4cef8eed18cd01a2610ad14c1f37daf800
Lassi Kortela <lassi@lassi.io>
2019-08-09 21:56:23 +0300

    Remove unnecessary MacOS-specific includes

--- c/socket.c
+++ c/socket.c
@@ -1,19 +1,16 @@
-#include <sys/select.h>
+#include <sys/types.h>
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
+#include <sys/select.h>
+#include <sys/time.h>
+
 #include <assert.h>
 #include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #include "dtypes.h"
-
-#if defined(MACOSX)
-#include <sys/time.h>
-#include <sys/select.h>
-#include <sys/types.h>
-#endif
 
 #include "socket.h"
 
