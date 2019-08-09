fe01d2faa10354cf9212f434eeff69435aa288c9
Lassi Kortela <lassi@lassi.io>
2019-08-10 02:02:41 +0300

    Simplify FreeBSD get_exename()

--- c/fs_freebsd.c
+++ c/fs_freebsd.c
@@ -6,12 +6,8 @@
 
 char *get_exename(char *buf, size_t size)
 {
-    int mib[4];
-    mib[0] = CTL_KERN;
-    mib[1] = KERN_PROC;
-    mib[2] = KERN_PROC_PATHNAME;
-    mib[3] = -1;
-    sysctl(mib, 4, buf, &size, 0, 0);
+    int mib[4] = { CTL_KERN, KERN_PROC, KERN_PROC_PATHNAME, -1 };
 
+    sysctl(mib, 4, buf, &size, 0, 0);
     return buf;
 }
