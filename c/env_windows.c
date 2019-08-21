5caf33758921e43cccb94d8257c8f93d07e4e30d
Lassi Kortela <lassi@lassi.io>
2019-08-21 22:15:19 +0300

    Stub builtin_environment_stack for Windows

--- c/env_windows.c
+++ c/env_windows.c
@@ -1,1 +1,42 @@
+#include <sys/types.h>
+
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <limits.h>
+#include <math.h>
+#include <setjmp.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "dtypes.h"
+#include "utils.h"
+#include "utf8.h"
+#include "ios.h"
+#include "socket.h"
+#include "timefuncs.h"
+#include "hashing.h"
+#include "htable.h"
+#include "htableh_inc.h"
+#include "bitvector.h"
+#include "os.h"
+#include "random.h"
+#include "llt.h"
+
+#include "flisp.h"
+
+#include "error.h"
+
+#include "argcount.h"
+
 const char *env_get_os_name(void) { return "windows"; }
+
+value_t builtin_environment_stack(value_t *args, uint32_t nargs)
+{
+    (void)args;
+    argcount("environment-stack", nargs, 0);
+    return FL_NIL;
+}
