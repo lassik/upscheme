7da26c717093cb208c347ae8f872c570019ee562
Lassi Kortela <lassi@lassi.io>
2019-10-14 13:25:35 +0300

    Update Solaris cfmakeraw() emulation
    Now it matches the OpenSolaris version of GNU GRUB.

--- c/os_sunos.c
+++ c/os_sunos.c
@@ -19,12 +19,10 @@ char *get_exename(char *buf, size_t size)
 
 void cfmakeraw(struct termios *t)
 {
-    t->c_iflag &= ~(IMAXBEL | IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR |
-                    IGNCR | ICRNL | IXON);
+    t->c_iflag &=
+    ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
     t->c_oflag &= ~OPOST;
     t->c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
     t->c_cflag &= ~(CSIZE | PARENB);
     t->c_cflag |= CS8;
-    t->c_cc[VMIN] = 1;
-    t->c_cc[VTIME] = 0;
 }
